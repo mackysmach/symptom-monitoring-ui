a1e827ff5fd544e7c5395b7f5e4744e549b54da3
mackysmach <90068336+mackysmach@users.noreply.github.com>
2024-02-04 21:50:07 +0700

    updateliff2

---
+++ app/page.js
@@ -0,0 +1,23 @@
+import liff from '@line/liff';
+
+function Main(){
+
+    liff
+    .init({
+        liffId: "2003132004-R8W9JPw8", // Use own liffId
+    })
+    .then(() => {
+        liff.login();
+        const profile = liff.getProfile();
+        console.log(profile);
+    })
+    .catch((err) => {
+        // Error happens during initialization
+        console.log(err.code, err.message);
+    });
+    
+    return(
+        <h1></h1>
+
+    );
+}
